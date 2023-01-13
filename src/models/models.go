f052215ec6c7187e9af0541100de4b6b6b2a72f2
ssssshel <angelarteaga989@gmail.com>
2023-01-13 15:50:52 -0500

    models

--- src/models/models.go
+++ src/models/models.go
@@ -1,7 +1,12 @@
 package models
 
+import (
+	"time"
+)
+
 type UserDataModel struct {
 	ID_USER              uint64 `gorm:"column:id_user; primaryKey"`
+	ID_USER_ROL          uint64 `gorm:"column:id_user_rol"`
 	USER_NAME            string `gorm:"column:user_name"`
 	USER_SURNAME         string `gorm:"column:user_surname"`
 	USER_DOCUMENT_TYPE   string `gorm:"column:user_document_type"`
@@ -9,4 +14,27 @@ type UserDataModel struct {
 	USER_CELLPHONE       string `gorm:"column:user_cellphone"`
 	USER_EMAIL           string `gorm:"column:user_email"`
 	USER_PASSWORD        string `gorm:"column:user_password"`
+}
+
+type UserOrderModel struct {
+	ID_USER_ORDER        uint64                 `gorm:"column:id_user_order"`
+	TOTAL_PRICE          uint32                 `gorm:"column:total_price"`
+	TOTAL_PRICE_WDISC    uint32                 `gorm:"column:total_price_wdisc"`
+	ORDER_DATE           time.Time              `gorm:"column:order_date"`
+	DELIVERY_DATE        time.Time              `gorm:"column:delivery_date"`
+	TOTAL_ITEMS_QUANTITY uint16                 `gorm:"column:total_items_quantity"`
+	PRODUCTS             map[string]interface{} `gorm:"column:products"`
+	SHIPPING_ADDRESS     map[string]interface{} `gorm:"column:shipping_address"`
+	SHIPPING_PRICE       uint32                 `gorm:"column:shipping_price"`
+	ID_TRANSACTION_PAY   string                 `gorm:"column:id_transaction_pay"`
+}
+
+type UserRoleModel struct {
+	ID_USER_ROL   uint64 `gorm:"column:id_user_rol "`
+	USER_ROL_NAME string `gorm:"column:user_rol_name"`
+}
+
+type OrderStateModel struct {
+	ID_ORDER_STATE uint64 `gorm:"column:id_order_state"`
+	ORDER_STATE    string `gorm:"column:order_state"`
 }
